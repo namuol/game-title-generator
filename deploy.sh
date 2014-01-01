f416af2f068338547548ef47af7f1335aa656f60
Louis Acresti <louis.acresti@gmail.com>
2014-01-01 18:12:38 -0500

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+grunt
+mkdir -p wat
+cp -r dist/* wat/.
+
+git checkout gh-pages
+cp -r wat/* .
+rm -rf wat
+
+git add *
+git commit -am 'auto-deploy'
+git push origin gh-pages
+
+git checkout master
+cake build
